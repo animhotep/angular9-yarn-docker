eac2e51cab93d23e96cb8858018f4191ba8d8d14
pasha hz <p.karpenko@sendpulse.com>
2021-01-08 10:28:47 +0200

    http test

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PostsService} from "../shared/services/posts.service";
+import {Observable} from 'rxjs';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  users;
+  users1;
 
-  constructor() { }
+  constructor(private postsService: PostsService, private http: HttpClient) {
+  }
 
   ngOnInit(): void {
+    this.users = this.http.get('https://jsonplaceholder.typicode.com/users');
+
+    this.http.get('https://jsonplaceholder.typicode.com/users').toPromise().then((r: any) => {
+      this.users1 = r.map(u => `${u.name} == ${u.email}`);
+    });
   }
 
 }
--- src/app/shared/services/posts.service.ts
+++ src/app/shared/services/posts.service.ts
@@ -2,13 +2,14 @@ import {Injectable} from '@angular/core';
 import gql from 'graphql-tag';
 import {Apollo} from 'apollo-angular';
 import {map} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class PostsService {
-  constructor(private apollo: Apollo) {
+  constructor(private apollo: Apollo, private http: HttpClient) {
   }
 
   getPosts() {
@@ -26,6 +27,13 @@ export class PostsService {
         }
       }`,
     }).pipe(map((r: any) => r.data.posts));
+  }
+
+  getPhotos() {
+    return this.http.get('https://jsonplaceholder.typicode.com/photos').subscribe(
+      data => data,
+      err => console.log(err)
+    );
   }
 
 }
